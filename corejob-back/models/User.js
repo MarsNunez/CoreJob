2cf5ab8a3abbafed2e4c74cbefa8c719b676e989
MarsNunez <mars.nunez.arenas@gmail.com>
2025-10-23 15:57:12 -0500

    Adding User Model

---
+++ corejob-back/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  full_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["client", "provider"],
+  },
+});
