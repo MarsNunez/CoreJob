75880ed33314dce0873fb098653e75d57ba04644
MarsNunez <mars.nunez.arenas@gmail.com>
2025-10-23 13:12:23 -0500

    Setup

---
+++ corejob-back/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3002;
+const DB_URL = process.env.DB_URL;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB_URL);
+    console.log("✅ Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`🟢 App running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+};
+
+startServer();
